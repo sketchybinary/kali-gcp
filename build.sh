78dc1676aa8425724b8f0988a4650ce3f204cc51
will.kline <will.kline@penguin>
2020-03-19 16:02:40 -0400

    Enable xrdp, call virt-sysprep to clean up the image

--- build.sh
+++ build.sh
@@ -9,18 +9,23 @@ tar xvf kali-linux-2020.1-vbox-amd64.ova
 qemu-img convert -f vmdk -O raw Kali-Linux-2020.1-vbox-amd64-disk001.vmdk disk.raw
 
 # Virt-Customize Magic
-
 virt-customize -a disk.raw \
                --run-command "apt-key adv --keyserver hkp://pool.sks-keyservers.net:80 --recv-keys 6A030B21BA07F4FB" \
-               --append-line "/etc/apt/sources.list.d/google-compute-engine.list:deb http://packages.cloud.google.com/apt google-compute-engine-buster-stable main" \
+               --write "/etc/apt/sources.list.d/google-compute-engine.list:deb http://packages.cloud.google.com/apt google-compute-engine-buster-stable main" \
                --run-command "apt update" \
                --uninstall gdm3 \
-               --install google-osconfig-agent,kali-defaults,kali-root-login,desktop-base,xfce4,xfce4-places-plugin,xfce4-goodies \
+               --install google-osconfig-agent,ufw,kali-defaults,kali-root-login,desktop-base,xfce4,xfce4-places-plugin,xfce4-goodies,xrdp \
                --run-command "systemctl enable ssh" \
                --run-command "systemctl enable google-osconfig-agent.service" \
-               --run-command "systemctl set-default graphical.target"
+               --run-command "systemctl set-default graphical.target" \
+               --run-command "systemctl enable xrdp-sesman" \
+               --run-command "systemctl enable xrdp" \
+               --run-command "ufw disable" \
                --edit "/etc/default/grub:s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/" \
-               --edit "/etc/default/grub:s/quiet/console=ttyS0,38400n8d/"
+               --edit "/etc/default/grub:s/quiet/console=ttyS0,38400n8d/" \
+               --run-command "/usr/sbin/update-grub"
+
+virt-sysprep -a disk.raw
 
 tar --format=oldgnu -Sczf Kali-2020.1-cloud-amd64.tar.gz disk.raw 
 gsutil cp  Kali-2020.1-cloud-amd64.tar.gz gs://darkwolf-image-uploads/Kali-2020.1-cloud-amd64.tar.gz
