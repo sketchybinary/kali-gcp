e85894fb602d415b0ae0cd2f60e288b03f3e4c8d
will.kline <will.kline@penguin>
2020-03-19 17:29:49 -0400

    Rearrange, leave out this ufw nonsense, the firewall was already down

--- build.sh
+++ build.sh
@@ -10,20 +10,19 @@ qemu-img convert -f vmdk -O raw Kali-Linux-2020.1-vbox-amd64-disk001.vmdk disk.r
 
 # Virt-Customize Magic
 virt-customize -a disk.raw \
+               --edit "/etc/default/grub:s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/" \
+               --edit "/etc/default/grub:s/quiet/console=ttyS0,38400n8d/" \
+               --run-command "/usr/sbin/update-grub" \
                --run-command "apt-key adv --keyserver hkp://pool.sks-keyservers.net:80 --recv-keys 6A030B21BA07F4FB" \
                --write "/etc/apt/sources.list.d/google-compute-engine.list:deb http://packages.cloud.google.com/apt google-compute-engine-buster-stable main" \
                --run-command "apt update" \
                --uninstall gdm3 \
-               --install google-osconfig-agent,ufw,kali-defaults,kali-root-login,desktop-base,xfce4,xfce4-places-plugin,xfce4-goodies,xrdp \
+               --install google-osconfig-agent,kali-defaults,kali-root-login,desktop-base,xfce4,xfce4-places-plugin,xfce4-goodies,xrdp \
                --run-command "systemctl enable ssh" \
                --run-command "systemctl enable google-osconfig-agent.service" \
                --run-command "systemctl set-default graphical.target" \
                --run-command "systemctl enable xrdp-sesman" \
-               --run-command "systemctl enable xrdp" \
-               --run-command "ufw disable" \
-               --edit "/etc/default/grub:s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/" \
-               --edit "/etc/default/grub:s/quiet/console=ttyS0,38400n8d/" \
-               --run-command "/usr/sbin/update-grub"
+               --run-command "systemctl enable xrdp"
 
 virt-sysprep -a disk.raw
 
