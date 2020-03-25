4f580a9fd54411d7e96e55e6b98bcb2294f46116
will.kline <will.kline@penguin>
2020-03-25 10:31:32 -0400

    Install docker-compose

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ tar xvf kali-linux-2020.1-vbox-amd64.ova
 qemu-img convert -f vmdk -O raw Kali-Linux-2020.1-vbox-amd64-disk001.vmdk disk.raw
 
 # Virt-Customize Magic
+# shellcheck disable=SC2016
 virt-customize -a disk.raw \
                --edit "/etc/default/grub:s/GRUB_TIMEOUT=5/GRUB_TIMEOUT=1/" \
                --edit "/etc/default/grub:s/quiet/console=ttyS0,38400n8d/" \
@@ -23,6 +24,9 @@ virt-customize -a disk.raw \
                --install kali-defaults,kali-root-login,desktop-base,xfce4,xfce4-places-plugin,xfce4-goodies,xrdp \
                --install google-osconfig-agent,python-google-compute-engine,python3-google-compute-engine,google-compute-engine-oslogin,google-compute-engine \
                --install docker-ce \
+               --run-command 'curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose' \
+               --run-command "chmod +x /usr/local/bin/docker-compose" \
+               --run-command "ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose" \
                --run-command "systemctl enable ssh" \
                --run-command "systemctl enable google-osconfig-agent.service" \
                --run-command "systemctl enable docker" \
